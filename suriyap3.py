95cda5e6f8949baa9fb1994e7aec2d1f821245e7
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2026-06-06 23:02:06 +0530

    Update suriyap3.py

--- suriyap3.py
+++ suriyap3.py
@@ -34,7 +34,7 @@ URLS = [
 sheet_id = "1bKrdupQL1LXAOXwmM1YsoN3xoGiMfS7pgzTkQFC3cjw"
 worksheet_names = [
     "p1","p2","p3","p4","p5","p6","p7","p8","p9","p10",
-    "p11","p12","p13","p14","p15","p16","p17","p18","p19","p20","p21","p22","p23","p24","p25","p26","p27","p28","p29"]
+    "p11","p12","p13","p14","p15","p16","p17","p18","p19","p20","p21","p22","p23","p24","p25"]
 
 def scrape_chartink(url, worksheet_name):
     print(f"\n🚀 Starting scrape for '{worksheet_name}'")
